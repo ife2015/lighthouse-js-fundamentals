262be63201d70f44f71bd15c5845488917e987da
Ife Olaifa <ifeolaifa5@gmail.com>
2020-07-24 18:29:08 +0000

    how to make functions

--- hello.js
+++ hello.js
@@ -1,7 +1,23 @@
-const sayHello = function (name) {
-  console.log("Hello, " + name); 
-};
+// const sayHello = function (name) {
+//   console.log("Hello, " + name); 
+// };
 
-sayHello("Julia");
-sayHello("Ife");
-sayHello("Bob");
+// sayHello("Julia");
+// sayHello("Ife");
+// sayHello("Bob");
+
+// console.log
+
+// const sayHelloToConsole = function (name) {
+//   console.log("Hello, " + name);
+// }
+
+// sayHelloToConsole('')
+
+// return 
+
+// const sayHelloToConsole = function (name) {
+//   return "Hello, " + name;
+//  }
+ 
+//  console.log(sayHelloToConsole('John'));
